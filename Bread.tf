ab9fe1be7e47261b389cd0a10311884e4b3eb96b
bootrob <mcintyrerobert@outlook.com>
2025-11-26 11:32:50 +0000

    FIX: MWT001 - adding resource

--- Bread.tf
+++ Bread.tf
@@ -2,3 +2,9 @@ output "hello_world" {
     description = "A simple hello world message"
     value       = "hello world"
 }
+
+resource "local_file" "bread_file" {
+    filename = "bread.txt"
+    content  = "This is some bread content."
+}
+
